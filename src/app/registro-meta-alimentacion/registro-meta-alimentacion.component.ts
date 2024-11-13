7bbbbc0d77910f502209d363f44b402aed7e4682
Alain Lanfranko <alain.lanfranko2808@gmail.com>
2024-11-13 02:34:01 -0500

    Corrigiendo errores de interfaces

--- src/app/registro-meta-alimentacion/registro-meta-alimentacion.component.ts
+++ src/app/registro-meta-alimentacion/registro-meta-alimentacion.component.ts
@@ -1,33 +1,38 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro-meta-alimentacion',
-  standalone: true,  // Indica que este es un componente independiente
-  imports: [CommonModule, ReactiveFormsModule], // Importa ReactiveFormsModule aquí
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './registro-meta-alimentacion.component.html',
   styleUrls: ['./registro-meta-alimentacion.component.scss']
 })
 export class RegistroMetaAlimentacionComponent {
   metaForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router) {
     // Inicialización del formulario reactivo
     this.metaForm = this.fb.group({
       descripcion: ['', Validators.required],
       calorias: ['', [Validators.required, Validators.min(0)]],
-      proteinas: ['', [Validators.required, Validators.min(0)]],
-      grasas: ['', [Validators.required, Validators.min(0)]],
-      carbohidratos: ['', [Validators.required, Validators.min(0)]]
+      proteinas: [0, [Validators.required, Validators.min(0)]],
+      grasas: [0, [Validators.required, Validators.min(0)]],
+      carbohidratos: [0, [Validators.required, Validators.min(0)]]
     });
+  }
+
+  // Método para redirigir a la página principal
+  goBack() {
+    this.router.navigate(['/principal']);
   }
 
   // Método para manejar el envío del formulario
   onSubmit() {
     if (this.metaForm.valid) {
       console.log('Formulario enviado:', this.metaForm.value);
-      // Puedes añadir lógica aquí para enviar los datos al backend, si es necesario
     } else {
       console.log('Formulario inválido');
     }
