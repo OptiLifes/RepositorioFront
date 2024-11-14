f9cae7a573e530b049f425482a99e7e1d73fe802
fab7cio <fabrialex1996@gmail.com>
2024-11-14 01:09:28 -0500

    Estructura del Frontend definidas y rutas corregidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './public/landing-page/landing-page.component';
 import { LoginPageComponent } from './authentication/login-page/login-page.component';
 import { RegisterPageComponent } from './authentication/register-page/register-page.component';
-import { PrincipalPageComponent } from './principal-page/principal-page.component';
-import { RegistroMetaAlimentacionComponent } from './registro-meta-alimentacion/registro-meta-alimentacion.component';
-import { RegistroAlimentoComponent } from './registro-alimento/registro-alimento.component';
-import { RegistroAguaComponent } from './registro-agua/registro-agua.component';
-import { RegistroSuenoComponent } from './registro-sueno/registro-sueno.component';
-import { ProfilePageComponent } from './profile/app/profile/profile-page/profile-page.component';
-import { EditProfileComponent } from './profile/app/profile/edit-profile/edit-profile.component';
-import { ChangeProfilePictureComponent } from './profile/app/profile/change-profile-picture/change-profile-picture.component';
+import { PrincipalPageComponent } from './layouts/principal-page/principal-page.component';
+import { RegistroMetaAlimentacionComponent } from './intranet/registro-meta-alimentacion/registro-meta-alimentacion.component';
+import { RegistroAlimentoComponent } from './intranet/registro-alimento/registro-alimento.component';
+import { RegistroAguaComponent } from './intranet/registro-agua/registro-agua.component';
+import { RegistroSuenoComponent } from './intranet/registro-sueno/registro-sueno.component';
+import { ProfilePageComponent } from './intranet/profile/profile-page/profile-page.component';
+import { EditProfileComponent } from './intranet/profile/edit-profile/edit-profile.component';
+import { ChangeProfilePictureComponent } from './intranet/profile/change-profile-picture/change-profile-picture.component';
 
 export function authGuard(): boolean {
   const isAuthenticated = localStorage.getItem('isAuthenticated');
@@ -20,7 +20,7 @@ export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
-  
+
   // Rutas protegidas
   { path: 'principal', component: PrincipalPageComponent, canActivate: [authGuard] },
   { path: 'profile', component: ProfilePageComponent, canActivate: [authGuard] },
